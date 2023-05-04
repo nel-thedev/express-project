112a8efd1a8018c5922addb6300e2cc908b8905c
nel-thedev <nelelias@gmail.com>
2023-05-04 18:16:21 -0400

    Fixed favorites in profile

--- routes/users.js
+++ routes/users.js
@@ -16,22 +16,15 @@ router.get('/profile', isLoggedIn, function (req, res, next) {
           title: 1,
           content: { $substr: ['$content', 0, 170] },
           createdAt: 1,
+          favorites: 1,
         },
       },
     })
     .populate({
       path: 'favorites',
       populate: { path: 'user' },
-      options: {
-        projection: {
-          title: 1,
-          content: { $substr: ['$content', 0, 170] },
-          createdAt: 1,
-        },
-      },
     })
     .then((foundUser) => {
-      console.log(foundUser);
       foundUser.posts = foundUser.posts.reverse();
       foundUser.favorites = foundUser.favorites.reverse();
       res.render('user/profile.hbs', { foundUser });
@@ -48,19 +41,13 @@ router.get('/public-profile/:id', isProfileOwner, (req, res, next) => {
           title: 1,
           content: { $substr: ['$content', 0, 170] },
           createdAt: 1,
+          favorites: 1,
         },
       },
     })
     .populate({
       path: 'favorites',
       populate: { path: 'user' },
-      options: {
-        projection: {
-          title: 1,
-          content: { $substr: ['$content', 0, 170] },
-          createdAt: 1,
-        },
-      },
     })
     .then((foundUser) => {
       foundUser.posts = foundUser.posts.reverse();
