8f75786bb5ad10679c0dbcf7f3051f200315923d
nel-thedev <nelelias@gmail.com>
2023-05-04 14:55:35 -0400

    Limited characters on homepage cards

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,11 @@ router.get('/', function (req, res, next) {
     // .sort({})
     // .limit(2)
     .then((allPosts) => {
-      allPosts = allPosts.reverse().slice(0, 10);
+      allPosts = allPosts.reverse().slice(0, 9);
+      allPosts = allPosts.map(post => ({
+        ...post.toObject(),
+        content: post.content.substring(0, 170)
+      }));
       res.render('index', { allPosts });
     })
     .catch((err) => console.log(err));
