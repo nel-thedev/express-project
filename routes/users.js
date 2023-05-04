1c7497e03544b610bf2df9fc5160e14349316722
nel-thedev <nelelias@gmail.com>
2023-05-04 17:17:13 -0400

    Deployed

--- routes/users.js
+++ routes/users.js
@@ -11,12 +11,29 @@ router.get('/profile', isLoggedIn, function (req, res, next) {
     .populate({
       path: 'posts',
       populate: { path: 'user' },
+      options: {
+        projection: {
+          title: 1,
+          content: { $substr: ['$content', 0, 170] },
+          createdAt: 1,
+        },
+      },
     })
     .populate({
       path: 'favorites',
       populate: { path: 'user' },
+      options: {
+        projection: {
+          title: 1,
+          content: { $substr: ['$content', 0, 170] },
+          createdAt: 1,
+        },
+      },
     })
     .then((foundUser) => {
+      console.log(foundUser);
+      foundUser.posts = foundUser.posts.reverse();
+      foundUser.favorites = foundUser.favorites.reverse();
       res.render('user/profile.hbs', { foundUser });
     });
 });
@@ -26,13 +43,29 @@ router.get('/public-profile/:id', isProfileOwner, (req, res, next) => {
     .populate({
       path: 'posts',
       populate: { path: 'user' },
+      options: {
+        projection: {
+          title: 1,
+          content: { $substr: ['$content', 0, 170] },
+          createdAt: 1,
+        },
+      },
     })
     .populate({
       path: 'favorites',
       populate: { path: 'user' },
+      options: {
+        projection: {
+          title: 1,
+          content: { $substr: ['$content', 0, 170] },
+          createdAt: 1,
+        },
+      },
     })
     .then((foundUser) => {
-      res.render('user/public-profile.hbs', { foundUser });
+      foundUser.posts = foundUser.posts.reverse();
+      foundUser.favorites = foundUser.favorites.reverse();
+      res.render('user/profile.hbs', { foundUser });
     })
     .catch((err) => console.log(err));
 });
